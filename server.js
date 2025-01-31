ee38ce2fecaaaadb0d82aacadd188cd451300f6e
Rohith Chowki <119583656+Rohithchowk@users.noreply.github.com>
2025-01-31 19:52:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const employeeController = require('./controllers/employeeController');
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/patientDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://sriraman895:sriraman895@cluster0.xnrax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('Error connecting to MongoDB:', err));
 
